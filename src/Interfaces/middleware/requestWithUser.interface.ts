d9b2a6e547d26748674c0ae0e9f7ef18d22cee75
aananthv <aananthv@outlook.com>
2020-09-15 16:10:59 +0530

    fix: (Interfaces) Make user optional

--- src/Interfaces/middleware/requestWithUser.interface.ts
+++ src/Interfaces/middleware/requestWithUser.interface.ts
@@ -2,7 +2,7 @@ import { Request } from "express";
 import User from "../model/user.interface";
 
 interface RequestWithUser extends Request {
-  user?: User;
+  user: User;
 }
 
 const isRequestWithUser = (req: Request | RequestWithUser): req is RequestWithUser => {
